6aefd627d5db4e9061630f41ab4d0dba0307f689
Aram <aramsuqiasyan@yandex.ru>
2019-09-22 22:55:00 +0400

    s

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var menuBtn = document.getElementsByClassName('menubtn')[0].onclick = function(){
     document.getElementById('menu').classList.toggle('open');
     document.body.classList.toggle('over');
-    document.getElementsByClassName('wrapper')[0].classList.toggle('open');
+    document.getElementsByClassName('wrapper')[0].classList.toggle('over');
 }
