547f25512e729137791a31995bf1fa4770fca992
Aram <aramsuqiasyan@yandex.ru>
2019-09-22 22:35:52 +0400

    overflow

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 var menuBtn = document.getElementsByClassName('menubtn')[0].onclick = function(){
     document.getElementById('menu').classList.toggle('open');
+    document.body.classList.toggle('over');
 }
